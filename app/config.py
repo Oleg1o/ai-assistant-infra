f1291c923dc3d60d011b1bbbcce797bec2b39668
Igor <i.ivanov@bsv.legal>
2025-07-01 13:27:43 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,3 +5,6 @@ MODEL_NAME = os.getenv("MODEL", "llama3")
 
 # Хост и порт Ollama-сервера (если вдруг вынесен в отдельный контейнер)
 OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://ollama:11434")
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "default_token")
+API_URL = os.getenv("API_URL", "http://api:8000/ask")
