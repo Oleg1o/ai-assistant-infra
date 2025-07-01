c6d8615906ef8fcdc5a8bc1fbd2cadf5e453188c
Igor <i.ivanov@bsv.legal>
2025-07-01 16:20:37 +0700

    add ollama to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from langchain_community.llms import Ollama
+
 app = FastAPI()
 
+llm = Ollama(model="llama3")
+
+@app.post("/ask")
+async def ask(request: Request):
+    data = await request.json()
+    question = data.get("question", "")
+
+    response = llm.invoke(question)
+
+    return {"answer": response}
 @app.get("/ask")
 def ask(question: str):
     return {"answer": f"Вы спросили: {question}"}
